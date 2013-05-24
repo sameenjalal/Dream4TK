55e9faec0e7fffebfa0d1419099b9af261149fa3
Sameen Jalal <sameenjalal@gmail.com>
2013-05-23 22:52:55 -0400

    Added lots of new stuff

---
+++ new/footer.php
@@ -0,0 +1,4 @@
+  <script src="js/jquery.js"></script>
+  <script src="js/foundation.min.js"></script>
+</body>
+</html>
---
+++ new/index.php
@@ -0,0 +1,3 @@
+<?php include 'header.php'; ?>
+yoson
+<?php include 'footer.php'; ?>
